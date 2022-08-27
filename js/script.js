9414f82492f15592b2754d43f1d151b222d89165
rav4353 <ravanthsri20@gmail.com>
2022-08-27 10:39:02 +0530

    hi

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//if
+
+var age = 20;
+         
+if( age > 18 ) {
+   console.log("Qualifies for driving");
+}
+
+//if...else statement
+
+
+var age = 15;
+         
+            if( age > 18 ) {
+               console.log("Qualifies for driving");
+            } else {
+               console.log("Does not qualify for driving");
+            }
+
+
+//if...else if... statement
+
+var book = "maths";
+            if( book == "history" ) {
+               console.log("History Book");
+            } else if( book == "maths" ) {
+               console.log("Maths Book");
+            } else if( book == "economics" ) {
+               console.log("Economics Book");
+            } else {
+               console.log("Unknown Book");
+            }
