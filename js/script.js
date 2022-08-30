773db36e110d1919b43ec2ceb565c85c357f88ac
rav4353 <ravanthsri20@gmail.com>
2022-08-30 10:35:01 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -1,32 +1,61 @@
-//if
+// //if
 
-var age = 20;
+// var age = 20;
          
-if( age > 18 ) {
-   console.log("Qualifies for driving");
-}
+// if( age > 18 ) {
+//    console.log("Qualifies for driving");
+// }
 
-//if...else statement
+// //if...else statement
 
 
-var age = 15;
+// var age = 15;
+// var rto = "pass";
          
-            if( age > 18 ) {
-               console.log("Qualifies for driving");
+//             if( age >= 18  && rto=="pass") {
+//                console.log("Qualifies for driving");
+//             }
+//             else {
+//                console.log("Fail")
+//             }
+
+
+// //if...else if... statement
+
+// var book = "maths";
+//             if( book == "history" ) {
+//                console.log("History Book");
+//             } else if( book == "maths" ) {
+//                console.log("Maths Book");
+//             } else if( book == "economics" ) {
+//                console.log("Economics Book");
+//             } else {
+//                console.log("Unknown Book");
+//             }
+
+
+
+            // 29/08
+
+          var a =300;
+          var b = 255;
+          var c = 1100;
+
+          if(a<b) {
+            if(b<c) {
+               console.log(c,b,a);
+            } else if(a<c) {
+               console.log(b,c,a);
             } else {
-               console.log("Does not qualify for driving");
+               console.log(b,a,c);
             }
-
-
-//if...else if... statement
-
-var book = "maths";
-            if( book == "history" ) {
-               console.log("History Book");
-            } else if( book == "maths" ) {
-               console.log("Maths Book");
-            } else if( book == "economics" ) {
-               console.log("Economics Book");
+          } else if (c<a) {
+            if (b<c) {
+               console.log(a,c,b);
             } else {
-               console.log("Unknown Book");
+               console.log(a,b,c);
             }
+         } else {
+               console.log(c,a,b)
+            }
+         
